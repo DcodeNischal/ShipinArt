3e123f02882a54762d78ee16d4900e1222b4b3d3
DcodeNischal <dnischal11@gmail.com>
2022-05-29 11:31:13 +0545

    GodZer0

---
+++ signin-artist.js
@@ -0,0 +1,19 @@
+const signup=document.querySelector(".signup");
+const login=document.querySelector(".login");
+const toggleMove=document.querySelector(".toggleMove");
+const signupp =document.querySelector(".signupp");
+const loginn=document.querySelector(".loginn");
+
+
+
+login.addEventListener("click",()=>{
+    toggleMove.classList.add("toggleMoveRight");
+    loginn.classList.add("loginform");
+    signupp.classList.remove("signupform");
+})
+
+signup.addEventListener("click",()=>{
+    toggleMove.classList.remove("toggleMoveRight");
+    loginn.classList.remove("loginform");
+    signupp.classList.add("signupform");
+})
