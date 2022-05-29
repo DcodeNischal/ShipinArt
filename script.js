518eb6dae4bf959e09565ec163d7cbf4fb79e005
DcodeNischal <dnischal11@gmail.com>
2022-05-29 16:17:09 +0545

    GodZer0

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function chooseportal(event){
+    document.getElementById("login-block-wrapper").style.display="flex";
+}
+function exitlogin(evt) {
+document.getElementById("login-block-wrapper").style.display ="none";
+}
