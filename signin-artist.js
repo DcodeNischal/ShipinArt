7e1c0cfff0ca4cb2ead0153a1cd1d4f6a48e341c
DcodeNischal <dnischal11@gmail.com>
2022-05-29 11:34:24 +0545

    Godzer0

--- signin-artist.js
+++ signin-artist.js
@@ -10,10 +10,12 @@ login.addEventListener("click",()=>{
     toggleMove.classList.add("toggleMoveRight");
     loginn.classList.add("loginform");
     signupp.classList.remove("signupform");
+    toggleMove.innerHTML="Login";
 })
 
 signup.addEventListener("click",()=>{
     toggleMove.classList.remove("toggleMoveRight");
     loginn.classList.remove("loginform");
     signupp.classList.add("signupform");
+    toggleMove.innerHTML="Signup";
 })
