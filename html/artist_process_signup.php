8b82f1e3427859d75910b1b3a1a386fb8596f194
Ujjwal Dhakal <70379407+Ujjwal-D@users.noreply.github.com>
2022-07-23 20:39:56 +0545

    Update artist_process_signup.php
    some commits

--- html/artist_process_signup.php
+++ html/artist_process_signup.php
@@ -1,4 +1,5 @@
 <?php
+    echo "entered in artist_process";
     //process the signup form
     if($_SERVER['REQUEST_METHOD'] == "POST")
     {
@@ -7,8 +8,8 @@
         $phone = $_POST['artistphone'];
         $address = $_POST['artistaddress'];
         $upassword = $_POST['artistpass'];
-        $citizenship_front = '';
-        $citizenship_back = '';
+        $citizenship_front = $_FILES['citizenshipFront'];
+        $citizenship_back = $_FILES['citizenshipBack'];
 
 
         //check for empty fields
@@ -18,9 +19,12 @@
         }
         else
         {
+                echo "\nreached not empty part";
             //check if email is already in use, if so, redirect to signup page
             include_once '../database/connect.php';
+            echo "\nreached connected database";                //
             $db = mysqli_select_db($connection, 'shipinArt');
+            include_once '../database/artist_world.php';
             $query = "SELECT * FROM artist WHERE artist_email = '$email'";
             $result = mysqli_query($connection, $query);
             if(mysqli_num_rows($result) > 0)
@@ -31,16 +35,15 @@
             {
                 //new user
                 //check for image
-
-                if(isset($_FILES['citizenshipFront']))
+                echo "\nreached new user";
+             /*   if(isset($_FILES['citizenshipFront']))
                 {
-                    if($_FILES['citizenshipFront']['name'] != '')
-                    {
+                    echo "reached inside citizenshipFront.";
                         $image_name = $_FILES['citizenshipFront']['name'];
                         $image_type = $_FILES['citizenshipFront']['type'];
                         $image_tmp_name = $_FILES['citizenshipFront']['tmp_name'];
                         $new_image_name = 'front_'.rand(1, 10000).'_'.$name.'_'.$image_name;
-                        $image_path = '../extra_files/'.$new_image_name;
+                        $image_path = 'extra_files/'.$new_image_name;
 
                         //check for image file type
                         $supported_types = array('image/jpeg', 'image/gif', 'image/png');
@@ -48,29 +51,29 @@
                         if(in_array($image_type, $supported_types))
                         {
                             echo 'image file supported.';
+                            if (move_uploaded_file($image_tmp_name, $image_path)) 
+                            {
+                                echo "reached at citizenship front";
+                                $citizenship_front = $new_image_name;
+                            }
+
                         }
                         else
                         {
                             header('Location: signin-artist.php?error=Image file type not supported' . '&name=' . $name . '&email=' . $email .'&phone'.$phone.'&address'.$address);
                             return;
                         }
-                  }
                 }
 
-                if (move_uploaded_file($image_tmp_name, $image_path)) 
-                {
-                    $citizenship_front = $new_image_name;
-                }
-
+               */
                 if(isset($_FILES['citizenshipBack']))
                 {
-                    if($_FILES['citizenshipBack']['name'] != '')
-                    {
+                
                         $image_name = $_FILES['citizenshipBack']['name'];
                         $image_type = $_FILES['citizenshipBack']['type'];
                         $image_tmp_name = $_FILES['citizenshipBack']['tmp_name'];
                         $new_image_name = 'back_'.rand(1, 10000).'_'.$name.'_'.$image_name;
-                        $image_path = '../extra_files/'.$new_image_name;
+                        $image_path = 'extra_files/'.$new_image_name;
 
                         //check for image file type
                         $supported_types = array('image/jpeg', 'image/gif', 'image/png');
@@ -78,18 +81,19 @@
                         if(in_array($image_type, $supported_types))
                         {
                             echo 'image file supported.';
+                            
                         }
                         else
                         {
                             header('Location: signin-artist.php?error=Image file type not supported' . '&name=' . $name . '&email=' . $email .'&phone'.$phone.'&address'.$address);
                             return;
                         }
-                  }
+                  
                 }
-
+                echo "reached citizenship back";
                 if (move_uploaded_file($image_tmp_name, $image_path)) 
                 {
-                    $citizenship_back = $new_image_name;
+                    $citizenship_back = $new_back_image_name;
                 }
 
                 //encrupt password
@@ -98,6 +102,7 @@
                 //insert user into database
                 $query = "INSERT INTO artist( artist_name, artist_email, artist_phone, artist_address, user_image,  artist_password, artist_front_citizenship, artist_back_citizenship) VALUES ('$name', '$email', '$phone', '$address', '$upassword', '$citizenship_front', '$citizenship_back')";
                 $result = mysqli_query($connection, $query);
+                echo "reached at insert file and inserted.";
                 
                 if($result)
                 {
@@ -113,7 +118,7 @@
     }
     else
     {
-        header("Location: signin-artist.php?error=Something went wrong");
+        header("Location: signin-artist.php");
     }
 
 ?>
